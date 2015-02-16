8008b4c5d6d3c6651c8588e9509a739d57e3929a
sanemat <o.gata.ken@gmail.com>
2015-02-16 22:48:25 +0900

    Initial commit

---
+++ lib/git/diff/parser.rb
@@ -0,0 +1,9 @@
+require "git/diff/parser/version"
+
+module Git
+  module Diff
+    module Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/git/diff/parser_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Git::Diff::Parser do
+  it 'has a version number' do
+    expect(Git::Diff::Parser::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
