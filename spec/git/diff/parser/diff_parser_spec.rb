9510fb3d5716c278884f13e90e5dd14a75cf71ae
sanemat <o.gata.ken@gmail.com>
2015-02-21 17:48:38 +0900

    test(parser): add empty whitespace

--- spec/git/diff/parser/diff_parser_spec.rb
+++ spec/git/diff/parser/diff_parser_spec.rb
@@ -58,6 +58,65 @@ describe Git::Diff::Parser::DiffParser do
    end
     EOS
     end
+    let(:file3) { 'lib/saddler/reporter/github/helper.rb' }
+    let(:body3) do
+      <<-'EOS'
+@@ -2,40 +2,21 @@ module Saddler
+   module Reporter
+     module Github
+       module Helper
+-        def initialize(output)
+-          @output = output
+-        end
+-
+-        def report(messages, _options)
+-          @output.puts messages
+-        end
+-
+-        # {"checkstyle"=>
+-        #    {"file"=>
+-        #       [{"error"=>
+-        #           [{"@column"=>"4",
+-        #             "@line"=>"22",
+-        #             "@message"=>"Assignment Branch Condition size for report is too high. [34.34/15]",
+-        #             "@severity"=>"info",
+-        #             "@source"=>"com.puppycrawl.tools.checkstyle.Metrics/AbcSize"},
+-        #            {"@column"=>"4",
+-        #             "@line"=>"22",
+-        #             "@message"=>"Cyclomatic complexity for report is too high. [10/6]",
+-        #             "@severity"=>"info",
+-        #             "@source"=>"com.puppycrawl.tools.checkstyle.Metrics/CyclomaticComplexity"},
+-        #            {"@column"=>"6",
+-        #             "@line"=>"39",
+-        #             "@message"=>"Use space after control keywords.",
+-        #             "@severity"=>"info",
+-        #             "@source"=>"com.puppycrawl.tools.checkstyle.Style/SpaceAfterControlKeyword"}],
+-        #         "@name"=>"/Users/sane/work/ruby-study/saddler/lib/saddler/cli.rb"},
+-        #        {"@name"=>"/Users/sane/work/ruby-study/saddler/lib/saddler/reporter.rb"}]}}
+-        def parse(xml)
+-          Nori
+-            .new(parser: :rexml)
+-            .parse(xml)
++        def concat_body(data)
++          buffer = []
++          files = data['checkstyle']['file'] ||= []
++          files.each do |file|
++            errors = file['error'] ||= []
++            errors.each do |error|
++              severity = error['@severity'] && error['@severity'].upcase
++              message = error['@message']
++              buffer << [severity, message].compact.join(': ')
++            end
++          end
++          buffer.join("\n")
+         end
+       end
+     end
+   end
+ end
++
+      EOS
+    end
     it 'returns parsed patches' do
       diff_body = File.read('spec/support/fixtures/d1bd180-c27866c.diff')
       patches = Git::Diff::Parser::DiffParser.parse(diff_body)
@@ -65,6 +124,10 @@ describe Git::Diff::Parser::DiffParser do
       expect(patches.size).to eq 4
       expect(patches[0].file).to eq file1
       expect(patches[0].body).to eq body1
+      expect(patches[1].file).to eq file2
+      expect(patches[1].body).to eq body2
+      expect(patches[2].file).to eq file3
+      expect(patches[2].body).to eq body3
     end
   end
 end
