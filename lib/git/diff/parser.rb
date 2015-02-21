d1735ff46a833a6dd99b09004416feeadbd69790
sanemat <o.gata.ken@gmail.com>
2015-02-21 16:40:48 +0900

    feat(parser): add diff parser from checkstyle_filter-git gem

--- lib/git/diff/parser.rb
+++ lib/git/diff/parser.rb
@@ -2,11 +2,14 @@ require 'git/diff/parser/version'
 require 'git/diff/parser/line'
 require 'git/diff/parser/patch'
 require 'git/diff/parser/patches'
+require 'git/diff/parser/diff_parser'
 
 module Git
   module Diff
     module Parser
-      # Your code goes here...
+      def self.parse(contents)
+        DiffParser.parse(contents)
+      end
     end
   end
 end
---
+++ lib/git/diff/parser/diff_parser.rb
@@ -0,0 +1,39 @@
+module Git
+  module Diff
+    module Parser
+      class DiffParser
+        def self.parse(contents)
+          body = false
+          file_name = ''
+          patch = []
+          lines = contents.lines
+          line_count = lines.count
+          parsed = Patches.new
+          lines.each_with_index do |line, count|
+            case line.chomp
+            when /^diff/
+              unless patch.empty?
+                parsed << Patch.new(patch.join("\n"), file: file_name)
+                patch.clear
+                file_name = ''
+              end
+              body = false
+            when /^\-\-\-/
+            when %r{^\+\+\+ b/(?<file_name>.*)}
+              file_name = Regexp.last_match[:file_name]
+              body = true
+            when /^(?<body>[\ @\+\-\\].*)/
+              patch << Regexp.last_match[:body] if body
+              if !patch.empty? && body && line_count == count + 1
+                parsed << Patch.new(patch.join("\n"), file: file_name)
+                patch.clear
+                file_name = ''
+              end
+            end
+          end
+          parsed
+        end
+      end
+    end
+  end
+end
