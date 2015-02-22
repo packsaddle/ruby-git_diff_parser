105e8e7394d3f443a0f845762c13ae301163e7f5
sanemat <o.gata.ken@gmail.com>
2015-02-22 15:57:17 +0900

    refactor(test): use module

--- spec/git_diff_parser/patch_spec.rb
+++ spec/git_diff_parser/patch_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
-describe GitDiffParser::Patch do
-  describe '#changed_lines' do
-    it 'returns lines that were modified' do
-      patch_body = File.read('spec/support/fixtures/patch.diff')
-      patch = GitDiffParser::Patch.new(patch_body)
+module GitDiffParser
+  describe Patch do
+    describe '#changed_lines' do
+      it 'returns lines that were modified' do
+        patch_body = File.read('spec/support/fixtures/patch.diff')
+        patch = Patch.new(patch_body)
 
-      expect(patch.changed_lines.size).to eq(3)
-      expect(patch.changed_lines.map(&:number)).to eq [14, 22, 54]
-      expect(patch.changed_lines.map(&:patch_position)).to eq [5, 13, 37]
-    end
+        expect(patch.changed_lines.size).to eq(3)
+        expect(patch.changed_lines.map(&:number)).to eq [14, 22, 54]
+        expect(patch.changed_lines.map(&:patch_position)).to eq [5, 13, 37]
+      end
 
-    context 'when body is nil' do
-      it 'returns no lines' do
-        patch = GitDiffParser::Patch.new(nil)
+      context 'when body is nil' do
+        it 'returns no lines' do
+          patch = Patch.new(nil)
 
-        expect(patch.changed_lines.size).to eq(0)
+          expect(patch.changed_lines.size).to eq(0)
+        end
       end
     end
   end
