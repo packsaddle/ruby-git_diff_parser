3b14ae79c3802eae4f7e3dcb016dde446151e1a1
sanemat <o.gata.ken@gmail.com>
2015-10-02 03:56:50 +0900

    feat(line): remove default, and use meaningful variable name

--- lib/git_diff_parser/line.rb
+++ lib/git_diff_parser/line.rb
@@ -7,17 +7,17 @@ module GitDiffParser
     # @!attribute [r] patch_position
     #   @return [Integer] line patch position
 
-    # @param options [Hash] this is not options
-    # @option options [Integer] :number line number (required)
-    # @option options [String] :content content (required)
-    # @option options [Integer] :patch_position patch position (required)
-    def initialize(options = {})
-      fail(ArgumentError('number is required')) unless options[:number]
-      fail(ArgumentError('content is required')) unless options[:content]
-      fail(ArgumentError('patch_position is required')) unless options[:patch_position]
-      @number = options[:number]
-      @content = options[:content]
-      @patch_position = options[:patch_position]
+    # @param params [Hash] required params
+    # @option params [Integer] :number line number (required)
+    # @option params [String] :content content (required)
+    # @option params [Integer] :patch_position patch position (required)
+    def initialize(params)
+      fail(ArgumentError('number is required')) unless params[:number]
+      fail(ArgumentError('content is required')) unless params[:content]
+      fail(ArgumentError('patch_position is required')) unless params[:patch_position]
+      @number = params[:number]
+      @content = params[:content]
+      @patch_position = params[:patch_position]
     end
 
     # @return [Boolean] true if line changed
