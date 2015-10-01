3a42c64193ad307d0f163158412fda2ad5ee0f8f
sanemat <o.gata.ken@gmail.com>
2015-10-01 21:43:00 +0900

    docs(line): update

--- lib/git_diff_parser/line.rb
+++ lib/git_diff_parser/line.rb
@@ -1,7 +1,16 @@
 module GitDiffParser
+  # parsed line
   class Line
     attr_reader :number, :patch_position
+    # @!attribute [r] number
+    #   @return [Integer] line number
+    # @!attribute [r] patch_position
+    #   @return [Integer] line patch position
 
+    # @param options [Hash] this is not options
+    # @option options [Integer] :number line number (required)
+    # @option options [String] :content content (required)
+    # @option options [Integer] :patch_position patch position (required)
     def initialize(options = {})
       fail(ArgumentError('number is required')) unless options[:number]
       fail(ArgumentError('content is required')) unless options[:content]
@@ -11,6 +20,7 @@ module GitDiffParser
       @patch_position = options[:patch_position]
     end
 
+    # @return [Boolean] true if line changed
     def changed?
       true
     end
