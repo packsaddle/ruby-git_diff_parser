225a192337e20c5a82bc8bdd96f78c3219258601
Vikas Panwar <vikaspanwar@VikasPanwarsMBP.att.net>
2016-12-09 22:58:20 -0800

    correlate merged commits

--- lib/git_diff_parser/commits_by_file.rb
+++ lib/git_diff_parser/commits_by_file.rb
@@ -3,12 +3,26 @@ module GitDiffParser
     class CommitsByFile
         def initialize(commit_log)
             commits = GitDiffParser.parselog(commit_log)
+            @merge_commits = {}
+            commits.each { |commit|
+              if !commit.mhash.nil?
+                orig_hash = commit.mhash.split(" ").last
+                @merge_commits[orig_hash] = commit
+              end
+            }
 
             @commits_by_file = Hash.new {|h,k| h[k]=[]}
             @prs = []
             commits.each { |commit|
                 commit.files.each { |file|
-                    @commits_by_file[file] << commit
+                    short_chash = commit.hash[0...7]
+                    if !@merge_commits[short_chash].nil?
+                      c = @merge_commits[short_chash]
+                      #c.comment << commit.comment 
+                      @commits_by_file[file] << c
+                    else
+                      @commits_by_file[file] << commit
+                    end
                 }
 
                 if !(commit.mhash.nil? || commit.mhash.empty?)
