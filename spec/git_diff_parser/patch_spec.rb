4456c9c0c272b5672d36680abdc2a2c0d206be9a
Joseph Buchma <josephbuchma@gmail.com>
2017-07-05 15:33:10 +0300

    test coverage for Patch#removed_lines

--- spec/git_diff_parser/patch_spec.rb
+++ spec/git_diff_parser/patch_spec.rb
@@ -21,6 +21,25 @@ module GitDiffParser
       end
     end
 
+    describe '#removed_lines' do
+      it 'returns lines that were modified' do
+        patch_body = File.read('spec/support/fixtures/patch.diff')
+        patch = Patch.new(patch_body)
+
+        expect(patch.removed_lines.size).to eq(7)
+        expect(patch.removed_lines.map(&:number)).to eq [11, 36, 37, 38, 39, 40, 48]
+        expect(patch.removed_lines.map(&:patch_position)).to eq [4, 21, 22, 23, 24, 25, 36]
+      end
+
+      context 'when body is nil' do
+        it 'returns no lines' do
+          patch = Patch.new(nil)
+
+          expect(patch.removed_lines.size).to eq(0)
+        end
+      end
+    end
+
     describe '#changed_line_numbers' do
       it 'returns line numbers that were modified' do
         patch_body = File.read('spec/support/fixtures/patch.diff')
